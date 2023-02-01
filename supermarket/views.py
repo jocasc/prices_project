90a8f401f9b02d4383fbaad510b0594418da2c17
jocasc <36902654+jocasc@users.noreply.github.com>
2023-02-01 21:21:42 +0000

    first page as promotions page

--- supermarket/views.py
+++ supermarket/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from datetime import datetime
 import requests
 
 
 def product_list(request):
     username = 'joca'
     password = 'ciencia$$'
-    base_url = 'http://joca.pythonanywhere.com/produtos/'
+    date = datetime.now()
+    base_url = f'http://joca.pythonanywhere.com/promocao/{date.day}/{date.month}/{date.year}/produto/'
     r = requests.get(f'{base_url}')
     products = r.json()
     return render(request, 'supermarket/index.html',{'products':products})
